d809d2117209cceb4ee61d9d11f6471425726a5c
Stephan J <r0990247@student.thomasmore.be>
2024-01-22 15:15:35 +0100

    vraag 8

---
+++ src/Employee.java
@@ -0,0 +1,10 @@
+import oefening2.IRole;
+import oefening2.Person;
+
+public abstract class Employee extends Person implements IRole{
+    public Employee(String name) {
+        super(name);
+    }
+
+    abstract boolean hasStudents();
+}
